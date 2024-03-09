3842633b251c78a25e690e0db02b2383e9b71937
David Inglody <inglody96@gmail.com>
2024-03-09 17:26:14 +0100

    route.ts fix

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
   //   verify webhook came from Stripe
   let event;
   try {
-    event = stripe.webhooks.constructorEvent(
+    event = stripe.webhooks.constructEvent(
       body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
